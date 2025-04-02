10010202cae60ad61f119ef97dfbbf6a9d0a6b3a
DESKTOP-3DOP2EC\ADMIN <rinkusalunke9@gmail.com>
2025-04-02 23:53:22 +0530

    1st commit

---
+++ src/main/java/com/Address.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Address {
+private String areaname;
+
+public String getAreaname() {
+	return areaname;
+}
+
+public void setAreaname(String areaname) {
+	this.areaname = areaname;
+}
+}
